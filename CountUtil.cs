761699c8c86518a822ec9eb3f919efc786f25523
a-let <aletinski@yahoo.com>
2016-09-21 10:56:28 -0400

    Add files via upload

---
+++ CountUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_thirteen
+{
+    class CountUtil
+    {
+        public static void count(Countable animal, int maxCount)
+        {
+            for (int i = 0; i < maxCount; i++)
+            {
+                animal.incrementCount();
+
+                Console.WriteLine(animal.getCountString() + " " + animal.GetType().ToString().Split('.')[1]);
+            }
+                
+        }
+
+        public static void count(Cloneable animal, int maxCount)
+        {
+            for (int i = 0; i < maxCount; i++)
+            {
+                animal.incrementCount();
+
+                Console.WriteLine(animal.getCountString() + " " + animal.getCloneName());
+            }
+
+        }
+    }
+}
---
+++ Countable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_thirteen
+{
+    interface Countable
+    {
+        void incrementCount();
+        void resetCount();
+        int getCount();
+        String getCountString();
+    }
+}
