60926da4522ba92681c92a0e7305e09ff9537bb9
a-let <aletinski@yahoo.com>
2016-09-26 09:28:15 -0400

    Add files via upload

---
+++ lab_thirteen/Cloneable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_thirteen
+{
+    interface Cloneable: Countable
+    {
+        Cloneable cloneAnimal(Cloneable animal);
+        void setCloneName(string name);
+        string getCloneName();
+    }
+}
---
+++ lab_thirteen/Sheep.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_thirteen
+{
+    class Sheep: Countable, Cloneable
+    {
+        private int _count = 0;
+        private string _name = "Blackie";
+
+        void Countable.incrementCount()
+        {
+            _count++;
+        }
+
+        void Countable.resetCount()
+        {
+            _count = 0;
+        }
+
+        int Countable.getCount()
+        {
+            return _count;
+        }
+
+        string Countable.getCountString()
+        {
+            return _count.ToString();
+        }
+
+        Cloneable Cloneable.cloneAnimal(Cloneable animal)
+        { 
+            animal = new Sheep();
+            animal.setCloneName("Dolly");
+
+            return animal;
+        }
+
+        void Cloneable.setCloneName(string name)
+        {
+            _name = name;
+        }
+
+        string Cloneable.getCloneName()
+        {
+            return _name;
+        }
+    }
+}
